80b7ba126e98b37253b7dd08df30274169fe1c9a
Yash Mehla <84175999+yashmehla@users.noreply.github.com>
2026-07-03 17:57:19 +0530

    Create g4gCheckSorted.java

---
+++ java/array/g4gCheckSorted.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isSorted(int[] arr) {
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>arr[i-1]){
+                continue;
+            }
+            else if(arr[i]<arr[i-1]){
+                return false;
+            
+            }
+        }
+        return true;
+        
+    }
+}
