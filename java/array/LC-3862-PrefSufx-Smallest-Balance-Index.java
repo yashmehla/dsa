64f1e05497c27621015284497244613804848d95
Yash Mehla <84175999+yashmehla@users.noreply.github.com>
2026-07-16 01:31:15 +0530

    Refactor smallestBalancedIndex method for clarity

--- java/array/LC-3862-PrefSufx-Smallest-Balance-Index.java
+++ java/array/LC-3862-PrefSufx-Smallest-Balance-Index.java
@@ -1,34 +1,59 @@
 class Solution {
     public int smallestBalancedIndex(int[] nums) {
         int n = nums.length;
-        long leftSum=0;
-        long largest = 0;
-        long[] leftsumArray= new long[n];
-        for(int i=0; i<n;i++){
-            leftsumArray[i]=leftSum;
-            leftSum+=nums[i];
-            if(leftsumArray[i]>largest){
-                largest=leftsumArray[i];
-            }
+        long[] leftsumArray = new long[n];
+        long leftSum = 0;
+        for (int i = 0; i < n; i++) {
+            leftsumArray[i] = leftSum;
+            leftSum += nums[i];
         }
-        long[] rightprodArray= new long[n];
-        long rightProd=1;
-        for (int i = nums.length - 1; i >= 0; i--){
-            rightprodArray[i]=rightProd;
-            if(rightProd>largest/nums[i]){
-            rightProd = largest +1;
-            }
-            else{
-                rightProd*=nums[i];
-            }
-        }
-        for(int i = 0; i <nums.length; i++){
-            if(leftsumArray[i]==rightprodArray[i]){
+        long rightProd = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (leftsumArray[i] == rightProd) {
                 return i;
             }
-            leftSum+=nums[i];
-
+            if (nums[i] == 0) break;
+            if (rightProd > leftsumArray[i] / nums[i]) {
+                break;
+            }
+            rightProd *= nums[i];
         }
         return -1;
     }
 }
+// FIRST SOLUTION
+// class Solution {
+//     public int smallestBalancedIndex(int[] nums) {
+//         int n = nums.length;
+//         long leftSum=0;
+//         long largest = 0;
+//         long[] leftsumArray= new long[n];
+//         for(int i=0; i<n;i++){
+//             leftsumArray[i]=leftSum;
+//             leftSum+=nums[i];
+//             if(leftsumArray[i]>largest){
+//                 largest=leftsumArray[i];
+//             }
+//         }
+//         long[] rightprodArray= new long[n];
+//         long rightProd=1;
+//         for (int i = nums.length - 1; i >= 0; i--){
+//             rightprodArray[i]=rightProd;
+//             if(rightProd>largest/nums[i]){
+//             rightProd = largest +1;
+//             }
+//             else{
+//                 rightProd*=nums[i];
+//             }
+//         }
+//         for(int i = 0; i <nums.length; i++){
+//             if(leftsumArray[i]==rightprodArray[i]){
+//                 return i;
+//             }
+//             leftSum+=nums[i];
+
+//         }
+//         return -1;
+//     }
+// }
