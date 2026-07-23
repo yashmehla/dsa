0b9d3fc1d7b31a3395aa3151850d6a93c38e4c0c
Yash Mehla <84175999+yashmehla@users.noreply.github.com>
2026-07-23 21:23:35 +0530

    Update LC-81-RotatedSortedSearchII.java

--- java/binarysearch/LC-81-RotatedSortedSearchII.java
+++ java/binarysearch/LC-81-RotatedSortedSearchII.java
@@ -1,26 +1,31 @@
 class Solution {
     public boolean search(int[] nums, int target) {
         int left = 0;
-        int right = nums.length - 1;
-        while (left <= right) {
+        int right = nums.length-1;
+        while(left<=right){
             int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
+            if(target==nums[mid]){
                 return true;
             }
-            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+            if(nums[left]==nums[mid] && nums[mid] == nums[right]){ // important difference
                 left++;
                 right--;
-            } else if (nums[left] <= nums[mid]) {
-                if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1;
-                } else {
+                continue;
+            }
+            if(nums[left]<=nums[mid]){
+                if(nums[left]<=target && target < nums[mid]){
+                    right  = mid - 1;
+                }
+                else{
                     left = mid + 1;
                 }
-            } else {
-                if (nums[mid] < target && target <= nums[right]) {
+            }
+            else{
+                if(nums[mid]<target && target <= nums[right]){
                     left = mid + 1;
-                } else {
-                    right = mid - 1;
+                }
+                else{
+                    right = mid -1;
                 }
             }
         }
