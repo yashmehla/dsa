13533703fb89ac7d32d26819d89c9fbef42712b8
Yash Mehla <84175999+yashmehla@users.noreply.github.com>
2026-07-24 19:18:32 +0530

    Implement binary search for peak element finding

---
+++ java/binarysearch/LC-162-BS-PeakElement.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        if(nums.length==1){
+            return 0;
+        }
+        if(nums[0]>nums[1]){
+            return 0;
+        }
+        if(nums[nums.length-1]>nums[nums.length-2]){
+            return nums.length-1;
+        }
+        int left = 1;
+        int right = nums.length-2;
+        while(left<=right){
+            int mid = left + (right-left) / 2;
+            if(nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1]){
+                return mid;
+            }
+            else if(nums[left]>nums[left-1] && nums[left]>nums[left+1]){
+                return left;
+            }
+            else{
+                left++;
+            }
+        }
+        return -1;
+    }
+}
