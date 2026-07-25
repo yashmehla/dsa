dd5d6bbf4296b51df20f99d8de8806e504381a87
Yash Mehla <84175999+yashmehla@users.noreply.github.com>
2026-07-25 15:19:23 +0530

    Create g4g-BS-NthRootofM.java

---
+++ java/binarysearch/g4g-BS-NthRootofM.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int nthRoot(int n, int m) {
+        for(int i =0; i<=m; i++){
+            if(Math.pow(i,n)==m){
+                return i;
+            }
+        }
+        return -1;
+        a
+    }
+}
