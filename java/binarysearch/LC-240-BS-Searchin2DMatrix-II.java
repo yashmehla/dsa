7829072f4650c35d719ccb2c494f2f351d173f61
Yash Mehla <84175999+yashmehla@users.noreply.github.com>
2026-08-11 14:08:38 +0530

    O(mlogn) complexity.

---
+++ java/binarysearch/LC-240-BS-Searchin2DMatrix-II.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for(int i = 0; i<matrix.length; i++){
+            int left = 0;
+            int right = matrix[i].length-1;
+            while (left<=right){
+                int mid = left + (right - left) / 2;
+                if(matrix[i][mid]==target){
+                    return true;
+                }
+                else if(matrix[i][mid]<target){
+                    left = mid + 1;
+                }
+                else{
+                    right = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+}
